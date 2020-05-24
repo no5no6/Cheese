c5bdb15d2013b8b2f0a7c82c77b03e4c2972ab28
yuanyang <yuan.yang@kuyun.com>
2020-05-24 11:34:36 +0800

    once 函数

--- Functional_programming.js
+++ Functional_programming.js
@@ -96,5 +96,31 @@ const every = (array, fn) => {
 // console.log(result);
 
 
-// once
+// 重点：箭头函数没有子集的 this 和 arguments 如果想用当前的 arguments 必须使用剩余参数表示法
+/**
+ * 实现 once 函数
+ * @param {} fn 
+ */
+const once = (fn) => {
+  let status = true;
+  return (...args) => {
+    if (status) {
+      status = false;
+      return fn.apply(this, args);
+    }
+  }
+}
+
+// 测试
+let pay = once((money) => {
+  console.log(money, 'money');
+  console.log(`共计${money}RMB`);
+});
+
+pay(5);
+pay(5);
+pay(5);
+pay(5);
+
+
 // reduce
