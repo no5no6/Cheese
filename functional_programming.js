1906a190042b0a7da6b76d6d9584d3a9f7e6f570
no5no6 <work_yuanyang@163.com>
2020-05-27 17:07:54 +0800

    update

--- functional_programming.js
+++ functional_programming.js
@@ -92,9 +92,9 @@
 // 实现 lodash flowRight 函数
 // const _ = require('lodash');
 
-// const reverse = arr => arr.reverse();
-// const first = arr => arr[0];
-// const toUpper = s => s.toUpperCase();
+const reverse = arr => arr.reverse();
+const first = arr => arr[0];
+const toUpper = s => s.toUpperCase();
 
 // const f = _.flowRight(toUpper, first, reverse);
 // console.log(f(['one', 'two', 'three']));
