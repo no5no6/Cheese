a98bd4d7943be4ff57270c413226eb3df765c7b5
no5no6 <work_yuanyang@163.com>
2020-06-02 10:21:59 +0800

    更新

--- functional_programming.js
+++ functional_programming.js
@@ -297,12 +297,12 @@ class IO {
     })
   }
 
-  constrcutor (fn) {
+  constructor(fn) {
     this._value = fn
   }
 
   map (fn) {
-    return IO.of(fp.flowRight(fn, this._value))
+    return new IO(fp.flowRight(fn, this._value))
   }
 
   join () {
@@ -315,20 +315,17 @@ class IO {
 }
 
 let readFile = (filename) => {
-  return IO.of(() => {
-    return fs.readFileSync(filename, 'utf-8')
-  })
+  return IO.of(fs.readFileSync(filename, 'utf-8'))
 }
 
 let print = (x) => {
-  return IO.of(() => {
+  return new IO(() => {
     console.log(x);
     return x;
   });
 }
 
-let r = readFile('package.json')
+readFile('package.json')
+  .map(fp.toUpper)
   .flatMap(print)
   .join()
-
-console.log(r);
